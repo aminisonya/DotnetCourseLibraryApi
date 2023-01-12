379cfaa636c8689fb36de1ed2c6f7320d7427709
Sonya Amini <soamini@microsoft.com>
2023-01-12 14:31:24 -0800

    add custom validation attribute

--- Models/CourseForManipulationDto.cs
+++ Models/CourseForManipulationDto.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
 
 namespace CourseLibrary.API.Models;
 
+[CourseTitleMustBeDifferentFromDescription]
 public abstract class CourseForManipulationDto
 {
     [Required(ErrorMessage = "You should fill out a title.")]
